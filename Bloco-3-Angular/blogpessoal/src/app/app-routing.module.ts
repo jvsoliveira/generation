16c8d9986fcb29346cf65d34553841e836b81085
Joao <jvsantosoliveira@gmail.com>
2021-12-07 10:11:57 -0300

    consumo api login e cadastro

--- Bloco-3-Angular/blogpessoal/src/app/app-routing.module.ts
+++ Bloco-3-Angular/blogpessoal/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -8,7 +9,9 @@ const routes: Routes = [
   {path: '', redirectTo:'login', pathMatch:'full'},
 
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'inicio', component: InicioComponent }
+
 
 ];
 
--- Bloco-3-Angular/blogpessoal/src/app/register/register.component.ts
+++ Bloco-3-Angular/blogpessoal/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  register(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha) {
+      alert('As senhas estão incorretas.')
+    } else {
+      this.authService.register(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
   }
 
 }
